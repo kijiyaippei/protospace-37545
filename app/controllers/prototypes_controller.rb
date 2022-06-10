4d36ddc17db0832344d14d3347529ba760f50935
kijiyaippei <kijiya0722@gmail.com>
2022-06-10 21:31:11 +0900

    first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,10 +51,9 @@ end
 
 private
 def prototype_params
-params.require(:prototype).permit(:title, :catch_copy, :concept)
+params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
 
 
-   
-
 end
+
